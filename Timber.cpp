737ffad8b93deff5880fcac548321b4fc0085449
Javier Carrasco Morente <jcmorente94@gmail.com>
2020-10-29 20:37:08 +0100

    Add some features

--- Timber.cpp
+++ Timber.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <sstream>
 
 using namespace sf;
 
@@ -58,7 +59,47 @@ int main()
 
 	Clock clock;
 
+	RectangleShape timeBar;
+	float timeBarStartWidth = 400;
+	float timeBarHeight = 80;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
 
+	Time gameTimeTotal;
+	float timeRemaining = 8.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+	bool pause = true;
+
+	int score = 0;
+
+	Text messageText;
+	Text scoreText;
+
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+
+	messageText.setFont(font);
+	scoreText.setFont(font);
+
+	messageText.setString("Press Enter to start!");
+	scoreText.setString("Score = 0");
+
+	messageText.setCharacterSize(75);
+	scoreText.setCharacterSize(100);
+
+	messageText.setFillColor(Color::White);
+	scoreText.setFillColor(Color::White);
+
+	FloatRect textRect = messageText.getLocalBounds();
+
+	messageText.setOrigin(textRect.left + textRect.width / 2.0f, 
+							textRect.top + textRect.height / 2.0f);
+
+	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+	scoreText.setPosition(20, 20);
 
 	while (window.isOpen())
 	{
@@ -67,96 +108,125 @@ int main()
 			window.close();
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Return))
+		{
+			pause = false;
+
+			score = 0;
+			timeRemaining = 8;
+		}
+
 		window.clear();
 
-		Time dt = clock.restart();
-
-		if (!beeActive)
+		if (!pause)
 		{
-			srand((int)time(0));
-			beeSpeed = (rand() % 200) + 200;
+			Time dt = clock.restart();
 
-			srand((int)time(0) * 10);
-			float height = (rand() % 500) + 500;
+			timeRemaining -= dt.asSeconds();
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
 
-			spriteBee.setPosition(2000, height);
-			beeActive = true;
-		}
-		else
-		{
-			spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()),
-									spriteBee.getPosition().y);
-
-			if (spriteBee.getPosition().x < -100)
+			if (timeRemaining <= 0.0f)
 			{
-				beeActive = false;
+				pause = true;
+
+				messageText.setString("Out of time!!");
+
+				FloatRect textRect = messageText.getLocalBounds();
+
+				messageText.setOrigin(textRect.left + textRect.width / 2.0f,
+					textRect.top + textRect.height / 2.0f);
 			}
-		}
 
-		if (!cloud1Active)
-		{
-			srand((int)time(0) * 10);
-			cloud1Speed = (rand() % 100);
-
-			srand((int)time(0) * 10);
-			float height = (rand() % 100);
-			spriteCloud1.setPosition(-200, height);
-			cloud1Active = true;
-		}
-		else
-		{
-			spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
-									spriteCloud1.getPosition().y);
-
-			if (spriteCloud1.getPosition().x > 1920)
+			if (!beeActive)
 			{
-				cloud1Active = false;
+				srand((int)time(0));
+				beeSpeed = (rand() % 200) + 200;
+
+				srand((int)time(0) * 10);
+				float height = (rand() % 500) + 500;
+
+				spriteBee.setPosition(2000, height);
+				beeActive = true;
 			}
-		}
-
-		if (!cloud2Active)
-		{
-			srand((int)time(0) * 20);
-			cloud2Speed = (rand() % 200);
-
-			srand((int)time(0) * 20);
-			float height = (rand() % 200) + 100;
-			spriteCloud2.setPosition(-200, height);
-			cloud2Active = true;
-		}
-		else
-		{
-			spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),
-				spriteCloud2.getPosition().y);
-
-			if (spriteCloud2.getPosition().x > 1920)
+			else
 			{
-				cloud2Active = false;
+				spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()),
+					spriteBee.getPosition().y);
+
+				if (spriteBee.getPosition().x < -100)
+				{
+					beeActive = false;
+				}
 			}
-		}
 
-		if (!cloud3Active)
-		{
-			srand((int)time(0) * 30);
-			cloud3Speed = (rand() % 300);
-
-			srand((int)time(0) * 30);
-			float height = (rand() % 300) + 200;
-			spriteCloud3.setPosition(-200, height);
-			cloud3Active = true;
-		}
-		else
-		{
-			spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
-				spriteCloud3.getPosition().y);
-
-			if (spriteCloud3.getPosition().x > 1920)
+			if (!cloud1Active)
 			{
-				cloud3Active = false;
-			}
-		}
+				srand((int)time(0) * 10);
+				cloud1Speed = (rand() % 100) + 50;
 
-		
+				srand((int)time(0) * 10);
+				float height = (rand() % 100);
+				spriteCloud1.setPosition(-200, height);
+				cloud1Active = true;
+			}
+			else
+			{
+				spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
+					spriteCloud1.getPosition().y);
+
+				if (spriteCloud1.getPosition().x > 1920)
+				{
+					cloud1Active = false;
+				}
+			}
+
+			if (!cloud2Active)
+			{
+				srand((int)time(0) * 20);
+				cloud2Speed = (rand() % 200) + 50;
+
+				srand((int)time(0) * 20);
+				float height = (rand() % 200) + 100;
+				spriteCloud2.setPosition(-200, height);
+				cloud2Active = true;
+			}
+			else
+			{
+				spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),
+					spriteCloud2.getPosition().y);
+
+				if (spriteCloud2.getPosition().x > 1920)
+				{
+					cloud2Active = false;
+				}
+			}
+
+			if (!cloud3Active)
+			{
+				srand((int)time(0) * 30);
+				cloud3Speed = (rand() % 300) + 50;
+
+				srand((int)time(0) * 30);
+				float height = (rand() % 300) + 200;
+				spriteCloud3.setPosition(-200, height);
+				cloud3Active = true;
+			}
+			else
+			{
+				spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
+					spriteCloud3.getPosition().y);
+
+				if (spriteCloud3.getPosition().x > 1920)
+				{
+					cloud3Active = false;
+				}
+			}
+
+			std::stringstream ss;
+			ss << "Score = " << score;
+			scoreText.setString(ss.str());
+
+		}
 
 		window.draw(spriteBackground);
 
@@ -168,8 +238,17 @@ int main()
 
 		window.draw(spriteBee);
 
+		window.draw(scoreText);
+
+		window.draw(timeBar);
+
+		if (pause)
+		{
+			window.draw(messageText);
+		}
+
 		window.display();
 	}
-
+	
 	return 0;
 }
